21c1a358ad1ce9a0507fe3ef83fad6317f7fe6fa
IlyaShi1rsh <shirshov.ilya@yandex.ru>
2020-04-21 14:00:36 +0300

    regularo4ki

---
+++ script.js
@@ -0,0 +1,14 @@
+// let ans = prompt("Name", "");
+// let reg = /n/;
+// console.log(ans.match(reg));
+// console.log(reg.test(ans));
+
+// let pass =('input pass');
+// console.log(pass.replace(/./g,"*"));
+
+// let ans = prompt('Введите число');
+// let reg = /\d/g;
+// console.log(ans.match(reg));
+
+let str = 'My name is R2D2';
+console.log(str.match(/\w\d\w\d/g));
